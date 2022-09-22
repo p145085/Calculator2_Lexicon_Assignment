9901007d653affc764ed69186b57284d4159c65b
emil.a.johansson <emil.a.johansson@gmail.com>
2022-09-22 22:41:16 +0200

    Add project files.

---
+++ Calc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2_Lexicon_Assignment
+{
+    internal class Calc
+    {
+        public static double Addition(double[] vals)
+        {
+            return vals.Sum();
+        }
+        public static double Subtraction(double[] vals)
+        {
+            double sum = vals[0];
+            for (int i = 1; i < vals.Length; i++)
+            {
+                sum = sum - vals[i];
+            }
+            return sum;
+        }
+        public static double Multiplication(double[] vals)
+        {
+            double sum = vals[0];
+            for (int i = 1; i < vals.Length; i++)
+            {
+                sum = sum * vals[i];
+            }
+            return sum;
+        }
+        public static double Division(double[] vals)
+        {
+            try
+            {
+                double sum = vals[0];
+                for (int i = 1; i < vals.Length; i++)
+                {
+                    sum = sum / vals[i];
+                }
+                return sum;
+            } catch (DivideByZeroException ex)
+            {
+                throw new DivideByZeroException(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator2_Lexicon_Assignment
+{
+    internal class Program
+    {
+        public static double? AskAndReceiveADouble()
+        {
+            try
+            {
+                Console.WriteLine("Enter a 'double' value: ");
+                string? input = Console.ReadLine();
+                double? val = double.Parse(input);
+                return val;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Hint: 'double' values are written with a comma(,) separator and not a period(.)");
+                Console.WriteLine("Hint: We're talking numbers, not characters.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Stacktrace: " + ex.StackTrace);
+                throw new Exception(ex.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            double[] vals = { -2, -2, 3, 0 };
+            Console.WriteLine(Calc.Addition(vals));
+            Console.WriteLine(Calc.Subtraction(vals));
+            Console.WriteLine(Calc.Multiplication(vals));
+            Console.WriteLine(Calc.Division(vals));
+            Console.WriteLine(AskAndReceiveADouble());
+        }
+    }
+}
